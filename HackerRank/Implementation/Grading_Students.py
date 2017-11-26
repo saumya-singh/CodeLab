0fbf8e46d9b41dc221f6689f1b6f773ed3be575b
saumya-singh <saumya.singh0993@gmail.com>
2017-11-26 22:39:27 +0530

    adding first file

---
+++ HackerRank/Implementation/Grading_Students.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import sys
+
+def solve(grades):
+    
+    for index, marks in enumerate(grades):
+        quotient = marks//5
+        diff = ((quotient + 1) * 5) - marks
+        if marks > 37 and diff < 3:
+            marks = (quotient + 1) * 5 
+            grades[index] = marks
+    return grades
+
+n = int(input().strip())
+grades = []
+grades_i = 0
+for grades_i in range(n):
+   grades_t = int(input().strip())
+   grades.append(grades_t)
+result = solve(grades)
+print ("\n".join(map(str, result)))
+
